6aac9613805d67ad3bd715828f20a43e07eeb324
Suraj-G-Rao <surajgrao0203@gmail.com>
2025-01-08 12:06:27 +0530

    Model Evaluation Completed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.WineQualityPrediction.pipeline.data_ingestion_pipeline import DataInges
 from src.WineQualityPrediction.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.WineQualityPrediction.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.WineQualityPrediction.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
-# from src.WineQualityPrediction.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
+from src.WineQualityPrediction.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -48,15 +48,15 @@ except Exception as e:
         logger.exception(e)
         raise e
 
-# STAGE_NAME = "Model evaluation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = ModelEvaluationTrainingPipeline()
-#    data_ingestion.initiate_model_evaluation()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
