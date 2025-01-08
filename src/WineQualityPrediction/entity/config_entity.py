a526edcaa7fee3827d0b2e8b0583f0423393ea1b
Suraj-G-Rao <surajgrao0203@gmail.com>
2025-01-08 11:01:22 +0530

    Data Transformation Completed

--- src/WineQualityPrediction/entity/config_entity.py
+++ src/WineQualityPrediction/entity/config_entity.py
@@ -7,9 +7,6 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
-    
-from dataclasses import dataclass
-from pathlib import Path
 
 @dataclass
 class DataValidationConfig:
@@ -17,3 +14,8 @@ class DataValidationConfig:
     STATUS_FILE:str
     unzip_data_dir:Path
     all_schema:dict
+    
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
